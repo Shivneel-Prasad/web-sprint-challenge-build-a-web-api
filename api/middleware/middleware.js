1773a00665633ac60448802d3df1f2e962f77ecc
Shivneel-Prasad <Shivam201020@gmail.com>
2022-02-10 19:41:11 -0800

    Initial Middleware Setup Complete

---
+++ api/middleware/middleware.js
@@ -0,0 +1,26 @@
+// Add middleware related to actions and project
+const AM = require('./actions-model')
+const PM = require('./projects-model')
+
+async function checkProjectId (req, res, next) {
+    
+}
+
+async function checkProject(req, res, next) {
+    
+}
+
+async function checkActionsId(req, res, next){
+    
+}
+
+async function checkActions(req, res, next){
+    
+}
+
+module.exports = {
+    checkProjectId,
+    checkProject,
+    checkActionsId, 
+    checkActions,
+}
