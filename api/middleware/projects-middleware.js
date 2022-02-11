ce8bfcfb48cb9f3500ca5906019698b8ecaafe9e
Shivneel-Prasad <Shivam201020@gmail.com>
2022-02-10 19:25:35 -0800

    Initial middleware setup

---
+++ api/middleware/projects-middleware.js
@@ -0,0 +1,15 @@
+// add middlewares here related to projects
+const PM = require('./projects-model')
+
+async function checkProjectId (req, res, next) {
+    
+}
+
+async function checkProject(req, res, next) {
+    
+}
+
+module.exports = {
+    checkProjectId,
+    checkProject
+}
